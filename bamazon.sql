cd382311a8de04f8bf4f61f35ceab38b13dad746
David Garza <dgarza0413@gmail.com>
2019-06-28 21:31:47 -0500

    table actively displaying

---
+++ bamazon.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+	product_id INTEGER AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    product_price DECIMAL(11, 2) NOT NULL,
+    product_department VARCHAR(30) NOT NULL,
+    product_stock INTEGER(11) NOT NULL,
+    PRIMARY KEY (product_id)
+);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("green-shirt", 10.99, "clothing", 100);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("blue-shirt", 12.99, "clothing", 100);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("red-shirt", 11.99, "clothing", 100);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("television", 349.99, "electronics", 50);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("laptop", 599.99, "electronics", 20);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("cellphone", 799.99, "electronics", 40);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("washing machine", 429.99, "appliances", 10);
+
+INSERT INTO products(product_name, product_price, product_department, product_stock)
+VALUES ("dryer machine", 529.99, "appliances", 30);
+
+
+CREATE TABLE departments(
+	department_id INTEGER AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(department_id)
+);
+
+INSERT INTO departments(department_name)
+VALUES ("electronics");
+
+INSERT INTO departments(department_name)
+VALUES ("clothing");
+
+INSERT INTO departments(department_name)
+VALUES ("appliances");
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,24 +1,106 @@
-// in this file we will display all of the items available for SVGFEFuncAElement
+require('dotenv').config()
+// in this file we will display all of the items available for purchase
+//we first can require our mysql database
+var mysql = require("mysql");
+var inquier = require("inquirer");
+var Table = require("cli-table");
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.BAMAZON_DB_PASSWORD,
+    database: "bamazon_db"
+})
 
+function purchaseItem() {
+    inquier.prompt([
+        {
+            type: "input",
+            message: "Select Item by Name",
+            name: "itemSelection"
+        },
+        {
+            type: "input",
+            message: "How many would you like?",
+            name: "quantitySelection"
+        }
+    ]).then(function (inquierResponse) {
+        // if (inquierResponse.itemSelection)
+        console.log(inquierResponse.itemSelection)
+        console.log(inquierResponse.quantitySelection)
+        if (inquierResponse.itemSelection !== product_id) {
+            console.log("Item is not available")
+        } else {
+            console.log("Item is available")
+        }
+    })
+}
+
+function readItems() {
+    console.log("Pulling all objects...\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["Product ID", "Product Name", "Product Price", "Product Stock", "Product Department"],
+            colWidths: [15, 20, 15, 15, 20]
+        })
+        // console.log(table.toString());
+        for (var i = 0; i < res.length; i++) {
+            table.push(
+                [`${res[i].product_id}`,
+                `${res[i].product_name}`,
+                `${res[i].product_price}`,
+                `${res[i].product_stock}`,
+                `${res[i].product_department}`]
+            )
+        }
+        console.log(table.toString());
+        connection.end();
+    })
+}
+
+function bamazonHome() {
+    inquier.prompt([
+        {
+            type: "list",
+            message: "Welcome to bamazon How may we help you",
+            choices: ["Purchase", "Option 1", "Option 2"],
+            name: "listOptions"
+        }
+    ]).then(function (inquierResponse) {
+        if (inquierResponse.listOptions === "Purchase") {
+            console.log("Purchase selected, pick your item")
+            readItems();
+            console.log(" ")
+            purchaseItem();
+        }
+    })
+}
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n")
+    bamazonHome();
+});
+
+
+
+//COMPLETE
 // includes the following
-
 // ids
 // names
 // prices of the products
 
+//COMPLETE
 // first it should ask them the id of the product for purchase
-
 // second should ask how many units
-
 
 // when order is placed a series of checks occur for availablity
 //     if not its insufficient quantity or out of stock
-
 //     if store has amount it fulfills order
 //         updates the database and reflects quantity
 //         present cost as well as to mulitply by the quantity purchased
 //          to the products product sales column
-
 
 // (challenge #3)
 // create a new MySQL table called departments that includes
