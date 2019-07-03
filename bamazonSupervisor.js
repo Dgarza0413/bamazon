bf2dcafeeb755a18a16db4467e65a9ef950d2938
David Garza <dgarza0413@gmail.com>
2019-07-02 21:19:17 -0500

    supervisor addition of more prompts

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -19,8 +19,66 @@ connection.connect(function (err) {
 
 function bamazonSupervisor() {
     inquier.prompt([
+        {
+            type: "list",
+            choices: ["view product sales by department", "create New Department"],
+            name: "menu"
+        }
+    ]).then(function (inquierResponse) {
+        // console.log(inquierResponse);
+        switch (inquierResponse.menu) {
+            case "view product sales by department":
+                console.log("view of product sales by department selected")
+                salesDepartment()
+                break;
+            case "create New Department":
+                console.log("create New Department selected")
+                newDepartment()
+                break;
+            default:
+                console.log("good Bye")
+        }
+    })
+}
+function salesDepartment() {
+    var query = "SELECT * FROM departments"
+    connection.query(query,
+        function (err, res) {
+            if (err) throw err;
+            var table = new Table({
+                head: ["department ID", "department name", "department overhead", "department sales"],
+                colWidths: [12, 20, 25, 20]
+            })
+            for (var i = 0; i < res.length; i++) {
+                console.log(res[i])
+                table.push(
+                    [
+                        `${res[i].department_id}`,
+                        `${res[i].department_name}`,
+                        `${res[i].department_overhead}`
+                    ]
+                )
+            }
+            console.log(table.toString())
+        })
+}
 
-    ])
+function newDepartment() {
+    inquier.prompt([
+        {
+            type: "input",
+            message: "create new department name",
+            name: "createDepartment"
+        },
+        {
+            type: "input",
+            message: "input overhead costs",
+            name: "overhead"
+        }
+    ]).then(function (inquierResponse) {
+        console.log(inquierResponse.createDepartment)
+        console.log(inquierResponse.overhead)
+    })
 }
 
 // (challenge #3)
@@ -29,7 +87,7 @@ function bamazonSupervisor() {
 // view product sales by department
     // displays a summarized table in their terminal/bash window
 
-//     | department_id | department_name | over_head_costs | product_sales | total_profit |
+// | department_id | department_name | over_head_costs | product_sales | total_profit |
 // | ------------- | --------------- | --------------- | ------------- | ------------ |
 // | 01            | Electronics     | 10000           | 20000         | 10000        |
 // | 02            | Clothing        | 60000           | 100000        | 40000        |
